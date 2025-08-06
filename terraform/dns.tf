44b87a15a82144d28a13cb723c8b2a31408f0be6
Khoa Tran <ovn6002@gmail.com>
2025-08-06 14:24:57 +0700

    feat(terraform): add Mailtrap DNS records for domain verification and email authentication
    - Added CNAME record for Mailtrap verification (mt86)
    - Added DKIM records (rwmt1._domainkey, rwmt2._domainkey)
    - Added DMARC TXT record
    - Added link tracking CNAME (mt-link)

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -9,3 +9,48 @@ resource "cloudflare_dns_record" "microservices_subdomain" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_dns_record" "mt86_verification" {
+  zone_id = var.cloudflare_zone_id
+  name    = "mt86"
+  type    = "CNAME"
+  content = "smtp.mailtrap.live"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "dkim_1" {
+  zone_id = var.cloudflare_zone_id
+  name    = "rwmt1._domainkey"
+  type    = "CNAME"
+  content = "rwmt1.dkim.smtp.mailtrap.live"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "dkim_2" {
+  zone_id = var.cloudflare_zone_id
+  name    = "rwmt2._domainkey"
+  type    = "CNAME"
+  content = "rwmt2.dkim.smtp.mailtrap.live"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "dmarc" {
+  zone_id = var.cloudflare_zone_id
+  name    = "_dmarc"
+  type    = "TXT"
+  content = "\"v=DMARC1; p=none; rua=mailto:dmarc@smtp.mailtrap.live; ruf=mailto:dmarc@smtp.mailtrap.live; rf=afrf; pct=100\""
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "mt_link_tracking" {
+  zone_id = var.cloudflare_zone_id
+  name    = "mt-link"
+  type    = "CNAME"
+  content = "t.mailtrap.live"
+  ttl     = 1
+  proxied = false
+}
